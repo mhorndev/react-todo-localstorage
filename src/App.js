2b9cc28b164d6f4dbc3d71711f6ad4c229fd4581
Mark Horn <mhorn.dev@gmail.com>
2020-10-31 04:40:25 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const icons = {
+    add: 'add_box',
+    checked: 'check_box',
+    unchecked: 'check_box_outline_blank'
+}
 
 function App() {
+  const [data, setData] = useState([])
+  const [text, setText] = useState("")
+
+  function onComplete(item) {
+    setData(data.filter(x => x.id !== item.id))
+  }
+
+  function onTextInput(e) {
+    setText(e.target.innerText)
+  }
+
+  function onNewTodo(item) {
+    if (text !== "") {
+      setData(existing => [...existing, {id:Date.now(), text:text, complete:false}]);
+      setText("");
+      document.getElementById('text-box').innerText = "";
+    }
+  }
+
+  function clearTodos() {
+    setData([])
+  }
+
+  function readTodos() {
+    setData(JSON.parse(localStorage.todos))
+  }
+
+  function writeTodos() {
+    localStorage.setItem('todos', JSON.stringify(data))
+  }
+
+  useEffect(() => {
+    if (!localStorage.todos) {
+      localStorage.setItem('todos', JSON.stringify([]))
+    }
+    readTodos()
+  }, [])
+
+  useEffect(() => {
+    writeTodos()
+  }, [data])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="title">
+        My Todo List
+      </div>
+
+      <div className="container">
+        {data &&
+          data.map((item) => {
+            return (
+              <div className="todo-item" key={item.id}>
+                <div className="todo-text">{item.text}</div>
+                <i className="material-icons checkbox" onClick={() => onComplete(item)}>
+                    {item.complete ? icons.checked : icons.unchecked}
+                </i>
+              </div>
+            )
+          })
+        }
+        <div className="todo-item new-item">
+          <div id="text-box" onInput={onTextInput}
+            spellCheck="false" 
+            contentEditable="true" 
+            placeholder="New todo"
+            className="new-item-text single-line" 
+          />
+          <i onClick={() => onNewTodo()}
+             className={"material-icons btnAdd " 
+             +(text !== "" ? "enabled": "disabled")} 
+          >
+            {icons.add}
+          </i>
+        </div>
+      </div>
+      <p/>
+      <button className="btnClear" onClick={clearTodos}>Clear todos</button>
+
+      <footer>
+        <p><code>Made with <i class="icon ion-heart secondary"></i> in React</code></p>
+      </footer>
     </div>
   );
 }
